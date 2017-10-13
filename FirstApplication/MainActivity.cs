3b5094389381c14e4701e1dda4f7a8979017d9ca
tornikegomareli <gomareli@itstep.org>
2017-10-13 19:34:47 +0400

    Simple calculator ui

---
+++ FirstApplication/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace FirstApplication
+{
+    [Activity(MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        int count2 = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+           
+
+
+
+        }
+    }
+}
+
